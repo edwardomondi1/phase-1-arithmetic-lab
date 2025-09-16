7ad79428389425aba37bf8b079befd00430d6669
edwardomondi1 <edward.omondi1@student.moringaschool.com>
2025-09-16 09:51:45 +0300

    message

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// Define variables num1 and num2 for multiplication
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Define random variable as a random integer greater than 0
+const random = Math.floor(Math.random() * 10) + 1;
+
+// Define variables num3 and num4 for modulus operation
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Define max variable to find the highest number in a set
+const max = Math.max(1, 5, 10, 20, 3);
